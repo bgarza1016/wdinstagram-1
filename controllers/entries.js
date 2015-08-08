7f5a157e60cc1d2b6e28588349bfddc2fbf7296a
Ezra Raez <ezRAez10@gmail.com>
2015-08-07 17:55:53 -0700

    Meghan's controller, model, and app.js

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,6 +1,7 @@
 // Controller for Entries
 
 // GET '/' - renders entries index page
-module.exports.renderEntriesIndex = function(req, res, next) {
-  res.render('entries/index', { title: 'WDInstagram' });
+
+module.exports.renderEntriesIndex = function(req, res, next){
+  res.render('entries/index', {title: 'WDInstagram,'});
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,41 @@
 var express = require('express');
 var router = express.Router();
-var entriesController = require('../controllers/entries');
+var entriesController = require('/controllers/entries');
 
-/* GET '/' - render entries index view */
-router.get('/', entriesController.renderEntriesIndex);
+router.get('/entries', entriesController.renderEntriesIndex);
+
+//Index
+router.get('/entries', function(req, res, next){
+  Entry.find(function(error, entries){
+    if (error) return res.send(error);
+    res.render('index', {title: 'entries', })
+  });
+});
+
+//NEW
+router.get('/entries/new', function(req, res, next){
+  respone.render('new', {title: 'New entry'});
+});
+
+//SHOW
+router.get('/entries/:id', function(req,res, next){
+  Entry.findOne({_id: req.params.id}, function(error,entry){
+  if (error) return res.send(error);
+  res.render('show', {title: entry.author, entry.entry_url, entry.date_taken, entry: entry });
+  })
+});
+
+//create
+router.post('/entries', function(req,res,next){
+  var entry = new Entry();
+  entry.author = req.body.author;
+  entry.entry_url = req.body.entry_url;
+  entry.date_taken = req.body.date_taken;
+
+  Entry.save(function(error){
+    if(error) return res.send(error);
+    res.redirect('/entries');
+  });
+});
 
 module.exports = router;
