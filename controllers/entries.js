fa4ce0fed8366e5d593afc1c777d6667186614ba
Ezra Raez <ezRAez10@gmail.com>
2015-08-09 22:49:37 -0700

    finished the backend sections of the wdinstagram

--- controllers/entries.js
+++ controllers/entries.js
@@ -3,6 +3,7 @@
 // GET '/' - renders entries index page
 var Entry = require('../models/entry');
 
+// Index view
 module.exports.renderEntriesIndex = function(req, res, next){
   Entry.find({}, function (err,entries))
   res.render(
@@ -14,6 +15,7 @@ module.exports.renderEntriesIndex = function(req, res, next){
         );
     });
 
+// Show view
 module.exports.renderEntriesShow = function(req, res, next) {
     Entry.findById(req.params.id, function(err,entry){
     res.render(
@@ -25,5 +27,43 @@ module.exports.renderEntriesShow = function(req, res, next) {
           }
       );
 
+    });
+};
+
+// New View
+module.exports.renderEntriesNew = function(req, res, next) {
+  res.render(
+    'entries/new',
+    {
+      title: 'WDInstagram',
+      show:  true
+    }
+  );
+};
+
+// Create handler
+module.exports.renderEntriesCreate = function(req, res, next) {
+  Entry.create(req.body.entry, function (err, entry) {
+    if (err) res.send('> '+ err);
+    res.redirect('/entries/' + entry.id);
+  });
+};
+
+// Likes handler
+module.exports.renderEntriesLike = function(req, res, next) {
+  Entry.findById(
+    req.params.id,
+    function (err, entry) {
+      if (err) res.send('> '+ err);
+        entry.likes++;
+        entry.save(function(err) {
+        if (err) console.log(err);
+      });
+
+      if (req.query.redirect === 'index') {
+        res.redirect('/entries');
+      } else {
+        res.redirect('/entries/' + entry.id);
+      }
     });
 };
--- models/entry.js
+++ models/entry.js
@@ -13,7 +13,7 @@ var formatDate = function(date_taken) {
   return moment(date_taken).format('MMM D, YYYY');
 };
 
-// New entry schema for posts
+// New entry schema for posts including likes
 var entrySchema = new Schema({
   author:     String,
   date_taken: {
@@ -21,7 +21,17 @@ var entrySchema = new Schema({
     default: Date.now,
     get:     formatDate
   },
-  photo_url:  String
+  photo_url:  String,
+  likes:      {
+    type:    Number,
+    default: 0
+  }
 });
+
+entrySchema.methods.html_formatted_likes = function() {
+  return '<i class="fa fa-heart"></i> ' + this.likes +
+         (this.likes === 1 ? ' like' : ' likes');
+};
+
 
 module.exports = mongoose.model('Entry', entrySchema);
